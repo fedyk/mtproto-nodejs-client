6b73d41ff53bca3323356abb3beec4849816d6f2
Andrii Fedyk <fedyk72@gmail.com>
2024-02-15 20:06:43 +0100

    Update transport.ts

--- src/transport.ts
+++ src/transport.ts
@@ -1,15 +1,19 @@
 import net from "node:net";
+import events from "node:events";
+import aesjs from "aes-js";
 import { Debugger } from "debug";
-import type { DC } from "./types.js";
-import { Obfuscated } from "./obfuscated.js"
 import { debug } from "./debug.js";
+import { getIntermediateBytes, getRandomBytes } from "./common.js";
+import type { DC } from "./types.js";
 
-export class Transport extends Obfuscated {
+export class Transport extends events.EventEmitter {
   dc: DC;
   debug: Debugger;
   destroyed: boolean;
   socket?: net.Socket;
   stream?: Uint8Array;
+  encryptAES?: aesjs.ModeOfOperation.ModeOfOperationCTR;
+  decryptAES?: aesjs.ModeOfOperation.ModeOfOperationCTR;
 
   constructor(dc: DC) {
     super();
@@ -27,6 +31,7 @@ export class Transport extends Obfuscated {
 
   destroy() {
     this.destroyed = true;
+
     this.debug('destroy');
 
     if (this.socket && !this.socket.destroyed) {
@@ -41,11 +46,7 @@ export class Transport extends Obfuscated {
   connect() {
     this.stream = new Uint8Array();
 
-    this.socket = net.connect(
-      this.dc.port,
-      this.dc.ip,
-      this.handleConnect
-    );
+    this.socket = net.connect(this.dc.port, this.dc.ip, this.handleConnect);
 
     this.socket.on('data', this.handleData);
     this.socket.on('error', this.handleError);
@@ -57,10 +58,10 @@ export class Transport extends Obfuscated {
   /**
    * not sure if data is Uint8Array
    */
-  async handleData(data: Uint8Array) {
+  handleData(data: Uint8Array) {
     const bytes = new Uint8Array(data);
 
-    const deobfuscatedBytes = await this.deobfuscate(bytes);
+    const deobfuscatedBytes = this.deobfuscate(bytes);
 
     if (!this.stream) {
       throw new Error("`this.stream` can't be empty");
@@ -119,8 +120,8 @@ export class Transport extends Obfuscated {
     }
   }
 
-  async handleConnect() {
-    const initialMessage = await this.generateObfuscationKeys();
+  handleConnect() {
+    const initialMessage = this.generateObfuscationKeys();
 
     this.socket?.write(initialMessage);
 
@@ -130,11 +131,85 @@ export class Transport extends Obfuscated {
   /**
    * not sure if data is Uint8Array
    */
-  async send(bytes: Uint8Array) {
-    const intermediateBytes = this.getIntermediateBytes(bytes);
-
-    const obfuscatedBytes = await this.obfuscate(intermediateBytes);
+  send(bytes: Uint8Array) {
+    const intermediateBytes = getIntermediateBytes(bytes);
+    const obfuscatedBytes = this.obfuscate(intermediateBytes);
 
     this.socket?.write(obfuscatedBytes);
+  }
+
+  // https://core.telegram.org/mtproto/mtproto-transports#transport-obfuscation
+  generateObfuscationKeys() {
+    let initBytes: Uint8Array;
+
+    while (true) {
+      initBytes = getRandomBytes(64);
+
+      const initDataView = new DataView(initBytes.buffer);
+
+      initDataView.setUint32(56, 4008636142, true);
+
+      if (initBytes[0] === 239) {
+        continue;
+      }
+
+      const firstInt = initDataView.getUint32(0, true);
+
+      if ([
+        1145128264,
+        1414745936,
+        542393671,
+        1230262351,
+        3722304989,
+        4008636142,
+        33620758,
+      ].includes(firstInt)) {
+        continue;
+      }
+
+      const secondInt = initDataView.getUint32(4, true);
+
+      if (secondInt === 0) {
+        continue;
+      }
+
+      break;
+    }
+
+    const initRevBytes = new Uint8Array(initBytes).reverse();
+
+    const encryptKey = initBytes.slice(8, 40);
+    const encryptIV = initBytes.slice(40, 56);
+
+    const decryptKey = initRevBytes.slice(8, 40);
+    const decryptIV = initRevBytes.slice(40, 56);
+
+    // @ts-ignore
+    this.encryptAES = new aesjs.ModeOfOperation.ctr(encryptKey, encryptIV);
+
+    // @ts-ignore
+    this.decryptAES = new aesjs.ModeOfOperation.ctr(decryptKey, decryptIV);
+
+    const encryptedInitBytes = this.obfuscate(initBytes);
+
+    initBytes.set(encryptedInitBytes.slice(56, 64), 56);
+
+    return initBytes;
+  }
+
+  obfuscate(bytes: aesjs.ByteSource) {
+    if (!this.encryptAES) {
+      throw new RangeError("`this.encryptAES` can't be empty");
+    }
+
+    return this.encryptAES.encrypt(bytes);
+  }
+
+  deobfuscate(bytes: aesjs.ByteSource) {
+    if (!this.decryptAES) {
+      throw new RangeError("`this.encryptAES` can't be empty");
+    }
+
+    return this.decryptAES.decrypt(bytes);
   }
 }
