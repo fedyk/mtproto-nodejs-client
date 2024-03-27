8aacb0bcc3ca28ba74a0021d2c76d95a65843751
Andrii Fedyk <fedyk72@gmail.com>
2024-03-27 10:31:01 +0100

    use different test phone number

--- src/mtproto.test.ts
+++ src/mtproto.test.ts
@@ -14,7 +14,7 @@ if (!process.env.API_HASH) {
 
 const api_id = Number(process.env.API_ID || 0)
 const api_hash = String(process.env.API_HASH || "")
-const phone_number = "9996621111"
+const phone_number = "9996621112"
 const phone_code = "22222"
 
 test.describe("MTProto", {
